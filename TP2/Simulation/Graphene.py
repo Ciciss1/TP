12dbd7713be1f6f18ea4cf4196539463e7c2e94e
Ciciss1 <alexis.escarmelle@epfl.ch>
2026-03-06 15:17:38 +0100

    uptade Graphene

--- TP2/Simulation/Graphene.py
+++ TP2/Simulation/Graphene.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 from numba import njit
 from shapely.geometry import Point, Polygon
-from scipy.spatial import cKDTree
+from scipy.spatial import cKDTree, Delaunay
 
 @njit
 def generate_graphene_lattice(L, a_CC = 1.42):
@@ -75,17 +76,16 @@ class GrapheneCrystal:
         mask[list(to_remove)] = False
         self.atoms = self.atoms[mask]
 
-    def build_graphene_bonds(self, r_min = 1.2, r_max = 1.8, max_neighbors = 3):
+    def build_graphene_bonds(self, r_min = 1.3, r_max = 2, max_neighbors = 3):
         tree = cKDTree(self.atoms)
         pairs = tree.query_pairs(r_max)
 
         neighbors = [[] for _ in range(len(self.atoms))]
         for i, j in pairs:
-            d = np.linalg.norm(self.atoms[i] - self.atoms[j])
-
-            if r_min < d < r_max:
-                neighbors[i].append((j, d))
-                neighbors[j].append((i, d))
+            r = np.linalg.norm(self.atoms[i] - self.atoms[j])
+            if r_min < r < r_max:
+                neighbors[i].append((j, r))
+                neighbors[j].append((i, r))
 
         self.bonds = []
         for i in range(len(self.atoms)):
@@ -97,17 +97,16 @@ class GrapheneCrystal:
             for j, d in neigh:
                 if i < j:
                     self.bonds.append((i, j))
+
         self.bonds = np.array(self.bonds, dtype=np.int32)
+        
 
     def build_polycrystal(self, a = 1.42):
         
-        base_lattice = generate_graphene_lattice(2*self.L, a_CC=a)
-        base_lattice += np.array([3*self.L/2, 3*self.L/2])
+        base_lattice = generate_graphene_lattice(3 * self.L / 2, a)
+        base_lattice += np.array([self.L, self.L])
 
         all_atoms = []
-
-        start = 4 * self.N
-        end = 5 * self.N
 
         for grain in range(len(self.all_points)):
             region_idx = self.vor.point_region[grain]
@@ -116,7 +115,7 @@ class GrapheneCrystal:
             if -1 in vertices or len(vertices) == 0:
                 continue
 
-            polygon = Polygon(self.vor.vertices[vertices]).buffer(1e-10)
+            polygon = Polygon(self.vor.vertices[vertices]).buffer(a * 0.2)
 
             theta = self.theta[grain % self.N]
             rot_atoms = rotate_atoms(base_lattice, theta)
@@ -135,16 +134,29 @@ class GrapheneCrystal:
         mask = (self.atoms[:, 0] >= 0) & (self.atoms[:, 0] <= self.L) & (self.atoms[:, 1] >= 0) & (self.atoms[:, 1] <= self.L)
         self.atoms = self.atoms[mask]
 
-        self.remove_close_atoms(a * 0.8)
-        self.build_graphene_bonds()                  
+        self.remove_close_atoms(a * 0.9)
+        self.build_graphene_bonds()
 
-    def plot(self):
+    def plot_atoms(self):
         plt.figure(figsize=(6,6))
         plt.scatter(self.atoms[:, 0], self.atoms[:, 1], s=1, color='black')
         plt.xlim(0, self.L)
         plt.ylim(0, self.L)
         plt.gca().set_aspect('equal')
         plt.title('Graphene Crystal')
+        plt.xlabel(r"$x$")
+        plt.ylabel(r"$y$")
+        plt.tight_layout()
+
+    def plot_bonds(self):
+        plt.figure(figsize=(6,6))
+        lines = [(self.atoms[i], self.atoms[j]) for i, j in self.bonds]
+        lc = LineCollection(lines, colors='black', linewidths=0.5)
+        plt.gca().add_collection(lc)
+        plt.xlim(0, self.L)
+        plt.ylim(0, self.L)
+        plt.gca().set_aspect('equal')
+        plt.title('Graphene Bonds')
         plt.xlabel(r"$x$")
         plt.ylabel(r"$y$")
         plt.tight_layout()
