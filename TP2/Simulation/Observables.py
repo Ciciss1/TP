6f1a142c9f5c7a18d100ebcab0b22802ce2fb80e
Ciciss1 <alexis.escarmelle@epfl.ch>
2026-03-22 19:30:24 +0100

    Observables

--- TP2/Simulation/Observables.py
+++ TP2/Simulation/Observables.py
@@ -1,8 +1,11 @@
 import numpy as np
-from numba import njit, typed, types
+from numba import njit
 
 @njit
 def lower_bound(arr, x):
+    '''
+    Find the index of the first element in arr that is greater than or equal to x
+    '''
     left, right = 0, arr.size
     while left < right:
         mid = (left + right) // 2
@@ -14,6 +17,9 @@ def lower_bound(arr, x):
 
 @njit
 def bin_index(r, bin_bounds):
+    '''
+    Find the index of the bin that r belongs to
+    '''
     if r < bin_bounds[0] or r >= bin_bounds[bin_bounds.size - 1]:
         return -1
     k = lower_bound(bin_bounds, r)
@@ -21,21 +27,32 @@ def bin_index(r, bin_bounds):
     
 @njit
 def compute_psi6(atoms, neighbors):
+    '''
+    Compute the local orientational order parameter
+    '''
     N = len(atoms)
     psi6 = np.zeros(N, dtype=np.complex128)
 
     for i in range(N):
+        nb = neighbors[i]
         sum_psi = 0.0 + 0.0j
-        for j in neighbors[i]:
-            dx = atoms[j, 0] - atoms[i, 0]
-            dy = atoms[j, 1] - atoms[i, 1]
+        cnt = 0
+        for j in range(len(nb)):
+            if nb[j] < 0:
+                continue
+            dx = atoms[nb[j], 0] - atoms[i, 0]
+            dy = atoms[nb[j], 1] - atoms[i, 1]
             theta = np.arctan2(dy, dx)
             sum_psi += np.exp(6j * theta)
-        psi6[i] = sum_psi / len(neighbors[i])
+            cnt += 1
+        psi6[i] = sum_psi / cnt if cnt > 0 else 0.0 + 0.0j
     return psi6            
 
 @njit
 def compute_orientational_correlation(psi6, coords, bin_bounds):
+    '''
+    Compute the orientational correlation function G6(r) 
+    '''
     N = len(psi6)
     num_bins = len(bin_bounds) - 1
     G6 = np.zeros(num_bins, dtype=np.float64)
@@ -44,15 +61,13 @@ def compute_orientational_correlation(psi6, coords, bin_bounds):
     for i in range(N):
         xi = coords[i, 0]
         yi = coords[i, 1]
-        psi6_i_re = psi6[i].real
-        psi6_i_im = psi6[i].imag
 
         for j in range(i, N):
             dx = xi - coords[j, 0]
             dy = yi - coords[j, 1]
             r = np.sqrt(dx * dx + dy * dy)
             b = bin_index(r, bin_bounds)
-            G6[b] += psi6_i_re * psi6[j].real + psi6_i_im * psi6[j].imag
+            G6[b] += psi6[i].real * psi6[j].real + psi6[i].imag * psi6[j].imag
             count[b] += 1
 
     for b in range(num_bins):
